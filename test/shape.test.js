b15477494f31d37dfb79f1e81dea7f89ab322f1a
Kevin <khampkevin@gmail.com>
2023-05-02 22:00:15 -0400

    Updated package.json and ran passing test

---
+++ test/shape.test.js
@@ -0,0 +1,29 @@
+const {Circle, Square, Triangle} = require('../lib/shape.js')
+
+describe('Circle', () => {
+    it('Renders fine', () => {
+        const shape = new Circle()
+        let color = ('red')
+        shape.setColor(color)
+
+        expect(shape.render()).toEqual(`<circle cx="150" cy="150" r=50" fill=${color}/>`)
+    })
+})
+describe('Square', () => {
+    it('Renders fine', () => {
+        const shape = new Square()
+        let color = ('red')
+        shape.setColor(color)
+
+        expect(shape.render()).toEqual(`<rect width="50" height="50" fill=${color}/>`)
+    })
+})
+describe('Triangle', () => {
+    it('Renders fine', () => {
+        const shape = new Triangle()
+        let color = ('red')
+        shape.setColor(color)
+
+        expect(shape.render()).toEqual(`<polygon points ="180,0 270,180 90,180" fill=${color}/>`)
+    })
+})
