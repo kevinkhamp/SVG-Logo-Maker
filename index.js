e5692515a387b458e9dea3a845e6cf08916107d4
Kevin <khampkevin@gmail.com>
2023-05-02 23:34:24 -0400

    Complete?

--- index.js
+++ index.js
@@ -4,31 +4,38 @@ const {Circle, Square, Triangle} = require('./lib/shape.js')
 
 class SVG {
     constructor() {
-        this.shapeElement = ''
-        this.textElement = ''
+        this.shape = ''
+        this.text = ''
     }
+    // render function for creating the SVG file down the line
     render() {
-        return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200"/>`
+        return `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+        ${this.shape}
+        ${this.text}
+        </svg>`
     }
-    setText(text,color){
-        return `<text xmlns="http://www.w3.org/2000/svg" x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    // setText function for setting the text in the SVG
+    setText(text,textColor){
+        this.text = `<text xmlns="http://www.w3.org/2000/svg" x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>`
     }
+    // setShape function to render the shape
     setShape(shape) {
-        this.shapeElement = shape.render()
+        this.shape = shape.render()
     }
 }
 
+// Prompt to ask users
 inquirer.prompt([
     {
         type: 'checkbox',
         message: 'What shape would you like?',
         choices: ['Square', 'Circle', 'Triangle'],
-        name: 'shape',
+        name: 'shapeEl',
     },
     {
         type: 'input',
         message: 'What color would you like your shape?',
-        name: 'shape-color',
+        name: 'shapeColor',
     },
     {
         type: 'input',
@@ -38,8 +45,38 @@ inquirer.prompt([
     {
         type: 'input',
         message: 'What color do you like?',
-        name: 'text-color'
+        name: 'textColor'
     }
 ])
-.then((response) =>
-fs.writeFile())
+// Taking responses from above to create our SVG
+.then(({text, textColor, shapeEl, shapeColor}) => {
+    // const didn't work. This allows us to set the shape and shape color for the SVG
+    let shape;
+    switch (shapeEl) {
+        case 'Square':
+            shape = new Square();
+            shape.setColor(shapeColor);
+            break;
+        case 'Circle':
+            shape = new Circle();
+            shape.setColor(shapeColor);
+            break;
+        default:
+            shape = new Triangle();
+            shape.setColor(shapeColor)
+    }
+    // Create an SVG
+    const svg = new SVG()
+    // Take the input from the prompts to set
+    svg.setText(text, textColor)
+    svg.setShape(shape)
+    // Should create the SVG file
+    return fs.writeFileSync('logo.svg', svg.render())
+})
+// If SVG was a success
+.then(() => console.log("SVG created"))
+// Error catcher
+.catch((err) => {
+    console.error(err)
+    console.log("Oops lol")
+})
